3aa6df1a23186d2872d94c5d71890e13bd569ead
Alexey Alexandrov <alex.androv37@yahoo.com>
2023-01-04 12:24:29 +0300

    afisha component

---
+++ src/components/AfishaItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="afisha-item border-b border-indigo-900 p-4 2xl:flex 2xl:justify-between 2xl:items-center">
+      <div class="flex flex-col md:flex-row md:items-start">
+        <div class="date flex md:flex-col md:items-center md:mr-8">
+          <p class="uppercase text-xs font-medium">{{item.day}}, {{item.time}}</p>
+          <p class="uppercase text-xs font-medium mr-1 md:hidden">,</p>
+          <p class="uppercase text-xs md:text-2xl font-medium md:font-semibold mr-1 md:mr-0">{{item.date}}</p>
+          <p class="uppercase text-xs md:text-sm font-medium">{{item.month}}</p>
+        </div>
+        <div class="show-desc py-6 2xl:py-0 md:pt-0 md:mt-4 ">
+          <p class="uppercase text-2xl font-semibold">{{item.band}}</p>
+          <p class="uppercase text-sm font-medium">{{item.desc}}</p>
+        </div>
+      </div>
+      <div class="flex justify-end">
+        <a :href="item.link" target="blank" class="buy-ticket uppercase hover:border-red-500 transition duration-200 border-2 rounded-3xl py-2 px-8 ml-full">купить билет</a>
+      </div>
+    </div>
+</template>
+
+// <script>
+export default {
+    props: {
+        item: {
+            type: Object,
+            required: true,
+        }
+    }
+}
+</script>
+
+<style scoped>
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,96 +1,22 @@
 <template>
   <div>
-    <div class="border-t border-b border-indigo-900 p-4 2xl:flex 2xl:justify-between 2xl:items-center">
-      <div class="flex flex-col md:flex-row md:items-start">
-        <div class="date flex md:flex-col md:items-center md:mr-8">
-          <p class="uppercase text-xs font-medium">ВТ, 19:00</p>
-          <p class="uppercase text-xs font-medium mr-1 md:hidden">,</p>
-          <p class="uppercase text-xs md:text-2xl font-medium md:font-semibold mr-1 md:mr-0">13</p>
-          <p class="uppercase text-xs md:text-sm font-medium">декабрь</p>
-        </div>
-        <div class="show-desc py-6 2xl:py-0 md:pt-0 md:mt-4 ">
-          <p class="uppercase text-2xl font-semibold">название группы</p>
-          <p class="uppercase text-sm font-medium">Короткое описание мероприятия</p>
-        </div>
-      </div>
-      <div class="flex justify-end">
-        <a href="" target="blank" class="buy-ticket uppercase hover:border-red-500 transition duration-200 border-2 rounded-3xl py-2 px-8 ml-full">купить билет</a>
-      </div>
-    </div>
-    <!-- <div class="flex flex-col border-b border-indigo-900 p-4">
-      <div class="flex">
-        <p class="uppercase">ВТ, 19:00</p>
-        <p class="uppercase mr-1 md:hidden">,</p>
-        <p class="uppercase mr-1">13</p>
-        <p class="uppercase">декабрь</p>
-        </div>
-      <div class="py-6">
-        <p class="uppercase text-2xl font-semibold">название группы</p>
-        <p class="uppercase text-sm font-medium">Короткое описание мероприятия</p>
-      </div>
-      <a href="" target="blank" class="buy-ticket uppercase hover:border-red-500 transition duration-200 border-2 rounded-3xl py-2 px-8 self-end">купить билет</a>
-    </div>
-    <div class="flex flex-col border-b border-indigo-900 p-4">
-      <div class="flex">
-        <p class="uppercase">ВТ, 19:00</p>
-        <p class="uppercase mr-1 md:hidden">,</p>
-        <p class="uppercase mr-1">13</p>
-        <p class="uppercase">декабрь</p>
-        </div>
-      <div class="py-6">
-        <p class="uppercase text-2xl font-semibold">название группы</p>
-        <p class="uppercase text-sm font-medium">Короткое описание мероприятия</p>
-      </div>
-      <a href="" target="blank" class="buy-ticket uppercase hover:border-red-500 transition duration-200 border-2 rounded-3xl py-2 px-8 self-end">купить билет</a>
-    </div>
-    <div class="flex flex-col border-b border-indigo-900 p-4">
-      <div class="flex">
-        <p class="uppercase">ВТ, 19:00</p>
-        <p class="uppercase mr-1 md:hidden">,</p>
-        <p class="uppercase mr-1">13</p>
-        <p class="uppercase">декабрь</p>
-        </div>
-      <div class="py-6">
-        <p class="uppercase text-2xl font-semibold">название группы</p>
-        <p class="uppercase text-sm font-medium">Короткое описание мероприятия</p>
-      </div>
-      <a href="" target="blank" class="buy-ticket uppercase hover:border-red-500 transition duration-200 border-2 rounded-3xl py-2 px-8 self-end">купить билет</a>
-    </div>
-    <div class="flex flex-col border-b border-indigo-900 p-4">
-      <div class="flex">
-        <p class="uppercase">ВТ, 19:00</p>
-        <p class="uppercase mr-1 md:hidden">,</p>
-        <p class="uppercase mr-1">13</p>
-        <p class="uppercase">декабрь</p>
-        </div>
-      <div class="py-6">
-        <p class="uppercase text-2xl font-semibold">название группы</p>
-        <p class="uppercase text-sm font-medium">Короткое описание мероприятия</p>
-      </div>
-      <a href="" target="blank" class="buy-ticket uppercase hover:border-red-500 transition duration-200 border-2 rounded-3xl py-2 px-8 self-end">купить билет</a>
-    </div>
-    <div class="flex flex-col border-b border-indigo-900 p-4">
-      <div class="flex">
-        <p class="uppercase">ВТ, 19:00</p>
-        <p class="uppercase mr-1 md:hidden">,</p>
-        <p class="uppercase mr-1">13</p>
-        <p class="uppercase">декабрь</p>
-        </div>
-      <div class="py-6">
-        <p class="uppercase text-2xl font-semibold">название группы</p>
-        <p class="uppercase text-sm font-medium">Короткое описание мероприятия</p>
-      </div>
-      <a href="" target="blank" class="buy-ticket uppercase hover:border-red-500 transition duration-200 border-2 rounded-3xl py-2 px-8 self-end">купить билет</a>
-    </div> -->
+    <afisha-item v-for="item in items" :item="item" :key="item.id"/>
   </div>
 </template>
 
 <script>
+import AfishaItem from '@/components/AfishaItem'
 export default {
   name: 'Home',
   components: {
-
-  }
+    AfishaItem,
+  },
+    data: () => ({
+      items: [
+        {id:1, day: 'ВТ', time: '19:00', date: 13, month: 'декабрь', band: 'название группы', desc: 'Короткое описание мероприятия', link: 'https://translate.google.com/?hl=ru'},
+        {id:2, day: 'ср', time: '18:00', date: 14, month: 'декабрь', band: 'ТНФ', desc: 'рок кованых сапог', link: 'https://tailwindcss.com/docs/align-items'},
+    ]
+    }),
 }
 </script>
 
@@ -98,19 +24,7 @@ export default {
 .buy-ticket:active {
   background: linear-gradient(99.26deg, #E278FB 6.53%, rgba(222, 81, 29, 0.97) 51.82%, rgba(234, 169, 40, 0.94) 89.27%);
 }
-/* @media (min-width:768px){
-  .date {
-    margin-right:32px;
-  }
-} */
-/* @media (min-width:768px){
-  .show-desc {
-    margin-top:19px;
-  }
-} */
-/* @media (min-width:768px){
-  .buy-ticket {
-    justify-self: end;
-  }
-} */
+.afisha-item:first-child {
+  border-top: 1px solid rgb(49 46 129);
+}
 </style>
